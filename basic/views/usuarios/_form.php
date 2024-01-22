5f37bed2830b0be6e085c6ff89cefacfb020d84a
MACASARG <macasarg@hotmail.es>
2024-01-22 13:59:24 +0100

    Visualizacion de formulario

--- basic/views/usuarios/_form.php
+++ basic/views/usuarios/_form.php
@@ -10,8 +10,16 @@ use yii\widgets\ActiveForm;
 
 <div class="usuarios-form">
 
-    <?php $form = ActiveForm::begin(); ?>
-
+<?php $form = ActiveForm::begin([
+        'id' => 'registro-form',
+        'layout' => 'horizontal',
+        'fieldConfig' => [
+            'template' => "{label}\n{input}\n{error}",
+            'labelOptions' => ['class' => 'col-lg-1 col-form-label mr-lg-3'],
+            'inputOptions' => ['class' => 'col-lg-3 form-control'],
+            'errorOptions' => ['class' => 'col-lg-7 invalid-feedback'],
+        ],
+    ]); ?>
     <?= $form->field($model, 'nombre')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'apellido1')->textInput(['maxlength' => true]) ?>
     <?= $form->field($model, 'apellido2')->textInput(['maxlength' => true]) ?>
