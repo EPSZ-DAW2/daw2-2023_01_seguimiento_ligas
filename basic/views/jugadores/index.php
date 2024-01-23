bcb283a7e0e7d2b860fbf6d5dcdbd1bc469b2677
iagogas <iagogas@usal.es>
2024-01-23 19:27:46 +0100

    pruebas fallo en css

--- basic/views/jugadores/index.php
+++ basic/views/jugadores/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <?php $this->registerCssFile('@web/css/jugadores.css'); ?>
+    <?php $this->registerCssFile('@web/css/site.css'); ?>
 
     <title>Tabla de Jugadores</title>
 </head>
