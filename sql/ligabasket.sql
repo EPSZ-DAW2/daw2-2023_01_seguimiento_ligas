cd0a039d3c8539baaee90df2240e45085fbc4546
MACASARG <93997392+MACASARG@users.noreply.github.com>
2024-01-24 20:02:52 +0100

    Update ligabasket.sql

--- sql/ligabasket.sql
+++ sql/ligabasket.sql
@@ -258,7 +258,7 @@ CREATE TABLE `usuarios` (
   `email` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `provincia` varchar(50) NOT NULL,
-  `id_rol` int(6) UNSIGNED ZEROFILL NOT NULL
+  `id_rol` int(6) UNSIGNED ZEROFILL NOT NULL,
   `username` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
