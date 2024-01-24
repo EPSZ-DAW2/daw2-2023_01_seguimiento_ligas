7ee82d137e88a168db6687ed4e48918ca1ee1f56
diegoiglesiases <diegoiglesiases@usal.es>
2024-01-24 18:40:45 +0100

    pruebas en la página de noticias

--- basic/views/noticias/index.php
+++ basic/views/noticias/index.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
-<html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <?php $this->registerCssFile('@web/css/noticias.css'); ?>
+
+        <title>Noticias</title>
+    </head>
     <body>
-        sadfasdf
+        <p>
+            lkasjdflkñjsadf
+        </p>
     </body>
 </html>
