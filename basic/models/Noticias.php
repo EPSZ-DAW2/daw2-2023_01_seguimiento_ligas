c99115b91a86df76302cc3dee16ac7953657d264
diegoiglesiases <diegoiglesiases@usal.es>
2024-02-02 18:09:16 +0100

    zzzzzz

--- basic/models/Noticias.php
+++ basic/models/Noticias.php
@@ -8,16 +8,15 @@ use Yii;
  * This is the model class for table "noticias".
  *
  * @property int $id
- * @property string $titular
- * @property string|null $texto_noticia
+ * @property string $titulo
+ * @property string|null $descripcion
  * @property string $autor
- * @property string $fecha_publicacion
+ * @property string $fecha
  * 
  * @property Imagenes $imagen
  */
 class Noticias extends \yii\db\ActiveRecord
 {
-
     /**
      * {@inheritdoc}
      */
@@ -26,4 +25,45 @@ class Noticias extends \yii\db\ActiveRecord
         return 'noticias';
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+
+            [['titulo', 'descripcion', 'autor', 'fecha'], 'required', 'message' => 'Este campo es obligatorio'],
+             // Otras reglas de validación según tus necesidades
+            //[['id', 'id_imagen'], 'required'],
+            [['autor', 'titulo', 'fecha'], 'string', 'max' => 50],
+            [['descripcion'], 'string', 'max' => 99999],
+        ];
+    }
+
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'titulo' => 'Titulo',
+            'descripcion' => 'Descripcion',
+            'autor' => 'Autor',
+            'fecha' => 'Fecha',
+        ];
+    }
+
+
+
+    /**
+     * Gets query for [[Imagen]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getImagen()
+    {
+        return $this->hasOne(Imagenes::class, ['id' => 'id_imagen']);
+    }
 }
--- basic/views/noticias/index.php
+++ basic/views/noticias/index.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-        <?php $this->registerCssFile('@web/css/noticias.css'); ?>
+    <?php 
+       # $this->registerCssFile('@web/css/noticias.css'); 
+    ?>
 
-        <title>Noticias</title>
-    </head>
-    <body>
-        <p>
-            lkasjdflkñjsadf
-        </p>
-    </body>
+    <title>Noticias</title>
+</head>
+<body>
+
+    <div class="marco">
+
+    <h2>Noticias</h2>
+
+    <table class="tabla">
+       
+    </table>
+    <br>
+
+    </div>
+</body>
 </html>
