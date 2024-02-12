2a0d24a4a86f5d34b9407694c0dadb0e9cbc5d03
JorgeAbella95 <jorgeabella@usal.es>
2024-02-12 12:04:58 +0100

    Terminada la parte de la interfaz Grafica de Jornadas

--- basic/views/jornadas/update.php
+++ basic/views/jornadas/update.php
@@ -3,38 +3,54 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-$this->title = " Actualizar Jornada " . $jornada->numero;
-$this->params['breadcrumbs'][] = ['label' => 'Temporada', 'url' => ['jornadas/index', 'id' => $jornada->id_temporada]];
-$this->params['breadcrumbs'][] = 'Actualizar';
+$this->title = " ACTUALIZAR DATOS DE LA JORNADA";
+//$this->params['breadcrumbs'][] = ['label' => 'Temporada', 'url' => ['jornadas/index', 'id' => $jornada->id_temporada]];
+//$this->params['breadcrumbs'][] = 'Actualizar';
 
 ?>
 
-<div class="equipos-update">
+
+<div class="contenido-cabecera">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?php $form = ActiveForm::begin(); ?>
+</div>
 
-    <?= $form->field($jornada, 'numero')->textInput([
+
+
+<div id="contenedor-principal">
+    <div class="marco">
+
+    <p class="PaginaDeInicio">Datos de la jornada</p>
+
+    <?php $form = ActiveForm::begin([
+            'enableAjaxValidation' => false,
+            'enableClientValidation' => true,
+        ]); ?>
+
+    <?= $form->field($jornada, 'numero', ['options' => ['class' => 'campoTitulo']])->textInput([
         'maxlength' => true,
         'type' => 'number', // Establecer el tipo como "number"
         'pattern' => '[0-9]*', // Expresión regular para aceptar solo números
+        'class' => 'campo',
     ]) ?>
-
-    <?= $form->field($jornada, 'fecha_inicio')->widget(\yii\jui\DatePicker::class, [
+    <br>
+    <?= $form->field($jornada, 'fecha_inicio', ['options' => ['class' => 'campoTitulo']])->widget(\yii\jui\DatePicker::class, [
         'dateFormat' => 'yyyy-MM-dd',
-        'options' => ['class' => 'form-control'],
+        'options' => ['class' => 'campo', 'placeholder' => 'Año-Mes-Dias'],
     ]) ?>
-
-    <?= $form->field($jornada, 'fecha_final')->widget(\yii\jui\DatePicker::class, [
+    <br>
+    <?= $form->field($jornada, 'fecha_final', ['options' => ['class' => 'campoTitulo']])->widget(\yii\jui\DatePicker::class, [
         'dateFormat' => 'yyyy-MM-dd',
-        'options' => ['class' => 'form-control'],
+        'options' => ['class' => 'campo', 'placeholder' => 'Año-Mes-Dias'],
     ]) ?>
+    <br>
 
-    <div class="form-group">
-        <?= Html::submitButton('Actualizar', ['class' => 'btn btn-primary']) ?>
-    </div>
+    <p>
+        <?= Html::submitButton('Actualizar', ['class' => 'botonFormulario']) ?>
+        <?= Html::a('Atras', ['view', 'id' => $jornada->id], ['class' => 'botonFormulario']) ?>
+    </p>
 
     <?php ActiveForm::end(); ?>
-
+    </div>
 </div>
--- basic/views/jornadas/view.php
+++ basic/views/jornadas/view.php
@@ -6,35 +6,48 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\Equipos */
 
-$this->title = "Jornada " . $jornada->numero;
-$this->params['breadcrumbs'][] = ['label' => 'Temporada', 'url' => ['jornadas/index', 'id' => $jornada->id_temporada]];
-$this->params['breadcrumbs'][] = $this->title;
+$this->title = "JORNADA " . $jornada->numero;
+//$this->params['breadcrumbs'][] = ['label' => 'Temporada', 'url' => ['jornadas/index', 'id' => $jornada->id_temporada]];
+//$this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="marco2">
-<div class="equipos-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Editar', ['update', 'id' => $jornada->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Eliminar', ['delete', 'id' => $jornada->id], [
-            'class' => 'btn btn-danger',
-            'data' => [
-                'confirm' => '¿Estás seguro de que deseas eliminar esta jornada?',
-                'method' => 'post',
-            ],
-        ]) ?>
-    </p>
+<div class="contenido-cabecera">
 
-    <?= DetailView::widget([
-        'model' => $jornada,
-        'attributes' => [
-            'id',
-            'numero',
-            'fecha_inicio',
-            'fecha_final',
-        ],
-    ]) ?>
+    <h1>DATOS DE LA <?= Html::encode($this->title) ?></h1>
 
 </div>
+
+<div id="contenedor-principal">
+    <div class="marco">
+
+        <p class="PaginaDeInicio">Datos de la Jornada</p>
+        <?= DetailView::widget([
+            'model' => $jornada,
+            'options' => ['class' => 'table table-bordered detail-view', 'style' => 'background-color: rgba(255, 255, 255, 0.8); border: 1px solid #000;'], // Clase, fondo blanco y bordes
+            'template' => "<tr><th style='width:20%; text-align: center; font-weight: bold;'>{label}</th><td style='width:80%; text-align: center;'>{value}</td></tr>", // Plantilla personalizada sin centrado
+            'attributes' => [
+                'id',
+                'numero',
+                'fecha_inicio',
+                'fecha_final',
+            ],
+        ]) ?>
+
+        
+<p>
+            <?= Html::a('Editar', ['update', 'id' => $jornada->id], ['class' => 'botonFormulario']) ?>
+            <?= Html::a('Eliminar', ['delete', 'id' => $jornada->id], [
+                'class' => 'botonFormulario',
+                'data' => [
+                    'confirm' => '¿Estás seguro de que deseas eliminar esta jornada?',
+                    'method' => 'post',
+                ],
+            ]) ?>
+            <?= Html::a('Atras', ['index', 'id' => $jornada->temporada->id], ['class' => 'botonFormulario']) ?>
+
+
+        </p>
+
+    </div>
 </div>
