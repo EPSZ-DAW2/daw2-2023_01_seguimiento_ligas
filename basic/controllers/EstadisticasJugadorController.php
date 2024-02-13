31442baa7c9ff6a14b99f7a40ef2c01036628d1d
iagogas <iagogas@usal.es>
2024-02-13 18:55:58 +0100

    algunos cambios en partidos/view.php, estadisticas_jugador y estadisticas_jugador_partido

--- basic/controllers/EstadisticasJugadorController.php
+++ basic/controllers/EstadisticasJugadorController.php
@@ -4,20 +4,27 @@ namespace app\controllers;
 
 use Yii;
 use app\models\EstadisticasJugador;
+use app\models\EstadisticasJugadorPartido;
 use app\models\Jugadores;
 use yii\web\Controller;
+use yii\data\ActiveDataProvider;
 
 class EstadisticasJugadorController extends Controller
 {
     public function actionIndex()
     {
-    // Configura el proveedor de datos con paginación
-    $estadisticasJugadores = EstadisticasJugador::find()->with(['equipo', 'jugador', 'temporada'])->all();
+        // Configura el proveedor de datos con paginación
+        $dataProvider = new ActiveDataProvider([
+            'query' => EstadisticasJugador::find()->with(['equipo', 'jugador', 'temporada']),
+            'pagination' => [
+                'pageSize' => 10, // Puedes ajustar el tamaño de la página según tus necesidades
+            ],
+        ]);
 
-    // Renderiza la vista y pasa el proveedor de datos como parámetro
-    return $this->render('index', [
-        'estadisticasJugadores' => $estadisticasJugadores,
-    ]);
+        // Renderiza la vista y pasa el proveedor de datos como parámetro
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
     }
 
 
@@ -77,5 +84,47 @@ class EstadisticasJugadorController extends Controller
         } else {
             throw new NotFoundHttpException('Las estadísticas del jugador solicitadas no existen.');
         }
+    }
+
+    public function actionActualizarEstadisticas()
+    {
+        // Obtener todos los jugadores
+        $jugadores = Jugadores::find()->all();
+
+        foreach ($jugadores as $jugador) {
+            // Obtener los registros de EstadisticasJugadorPartido para el jugador actual
+            $estadisticasJugadorPartido = EstadisticasJugadorPartido::find()
+                ->where(['id_jugador' => $jugador->id])
+                ->andWhere(['>=', 'minutos', 0])
+                ->all();
+
+            $totalPuntos = 0;
+            $totalRebotes = 0;
+            $totalAsistencias = 0;
+
+            foreach ($estadisticasJugadorPartido as $estadistica) {
+                $totalPuntos += $estadistica->puntos;
+                $totalRebotes += $estadistica->rebotes;
+                $totalAsistencias += $estadistica->asistencias;
+            }
+
+            // Calcular las medias
+            $mediaPuntos = count($estadisticasJugadorPartido) > 0 ? $totalPuntos / count($estadisticasJugadorPartido) : 0;
+            $mediaRebotes = count($estadisticasJugadorPartido) > 0 ? $totalRebotes / count($estadisticasJugadorPartido) : 0;
+            $mediaAsistencias = count($estadisticasJugadorPartido) > 0 ? $totalAsistencias / count($estadisticasJugadorPartido) : 0;
+
+            // Actualizar la entrada en EstadisticasJugador
+            $estadisticaJugador = EstadisticasJugador::find()->where(['id_jugador' => $jugador->id])->one();
+
+            if ($estadisticaJugador !== null) {
+                $estadisticaJugador->puntos = $mediaPuntos;
+                $estadisticaJugador->rebotes = $mediaRebotes;
+                $estadisticaJugador->asistencias = $mediaAsistencias;
+                $estadisticaJugador->save();
+            }
+        }
+
+        // Redirigir a la página de índice u otra página según corresponda
+        return $this->redirect(['index']);
     }
 }
--- basic/views/estadisticas-jugador/index.php
+++ basic/views/estadisticas-jugador/index.php
@@ -3,6 +3,8 @@ use yii\grid\GridView;
 use yii\helpers\Html;
 use yii\grid\ActionColumn;
 use yii\helpers\Url;
+
+if (Yii::$app->user->isGuest ||(Yii::$app->user->identity->id_rol != 1 && Yii::$app->user->identity->id_rol != 5)) {
 ?>
 
 <!DOCTYPE html>
@@ -103,41 +105,97 @@ use yii\helpers\Url;
 </div>
 
         <div class="marco">
-            <table class="tabla">
-                <thead class="cabecera filas">
-                    <tr>
-                        <th>ID Jugador</th>
-                        <th>ID Equipo</th>
-                        <th>ID Temporada</th>
-                        <th>Partidos Jugados</th>
-                        <th>Puntos</th>
-                        <th>Rebotes</th>
-                        <th>Asistencias</th>
-                        <th>Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <?php foreach ($estadisticasJugadores as $estadistica): ?>
-                        <tr class="filas">
-                            <td class="filas"><?= $estadistica->jugador->nombre ?></td>
-                            <td class="filas"><?= $estadistica->equipo->nombre ?></td>
-                            <td class="filas"><?= $estadistica->temporada->texto_de_titulo ?></td>
-                            <td class="filas"><?= $estadistica->partidos_jugados ?></td>
-                            <td class="filas"><?= $estadistica->puntos ?></td>
-                            <td class="filas"><?= $estadistica->rebotes ?></td>
-                            <td class="filas"><?= $estadistica->asistencias ?></td>
-                            <td class="filas">
-                                <!-- Botones de Acciones -->
-                                <?= \yii\helpers\Html::a('Editar', ['update', 'id' => $estadistica->id], ['class' => 'botonPequeño']) ?>
-                                <?= \yii\helpers\Html::a('Ver', ['view', 'id' => $estadistica->id], ['class' => 'botonPequeño']) ?>
-                                <?= \yii\helpers\Html::a('Eliminar', ['delete', 'id' => $estadistica->id], ['class' => 'botonPequeño', 'data' => ['confirm' => '¿Estás seguro de que deseas eliminar esta estadística?']]) ?>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </tbody>
-            </table>
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => [
+                    [
+                        'label' => 'Nombre', // Etiqueta de la columna
+                        'attribute' => 'jugador.nombre', // Utiliza el nombre del jugador
+                    ],
+                    [
+                        'label' => 'Equipo', // Etiqueta de la columna
+                        'attribute' => 'equipo.nombre', // Utiliza el nombre del equipo
+                    ],
+                    [
+                        'label' => 'Temporada', // Etiqueta de la columna
+                        'attribute' => 'temporada.texto_de_titulo', // Utiliza el texto de título de la temporada
+                    ],
+                    'partidos_jugados',
+                    'puntos',
+                    'rebotes',
+                    'asistencias',
+                ],
+            ]); ?>
         </div>
     </div>
 </body>
 </html>
+
+<?php
+} else { ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php $this->registerCssFile('@web/css/site.css'); ?>
+    <title>Tabla de Estadísticas de Jugadores</title>
+</head>
+<body>
+    <div class="contenido-cabecera">
+        <h1>ESTADISTICA DE JUGADORES</h1>
+    </div>
+
+        <div class="marco">
+            <?= GridView::widget([
+                'dataProvider' => $dataProvider,
+                'columns' => [
+                    [
+                        'label' => 'Nombre', // Etiqueta de la columna
+                        'attribute' => 'jugador.nombre', // Utiliza el nombre del jugador
+                    ],
+                    [
+                        'label' => 'Equipo', // Etiqueta de la columna
+                        'attribute' => 'equipo.nombre', // Utiliza el nombre del equipo
+                    ],
+                    [
+                        'label' => 'Temporada', // Etiqueta de la columna
+                        'attribute' => 'temporada.texto_de_titulo', // Utiliza el texto de título de la temporada
+                    ],
+                    'partidos_jugados',
+                    'puntos',
+                    'rebotes',
+                    'asistencias',
+                    [
+                        'class' => ActionColumn::class,
+                        'template' => '{update} {view} {delete}',
+                        'buttons' => [
+                            'update' => function ($url, $model, $key) {
+                                return Html::a('Editar', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']);
+                            },
+                            'view' => function ($url, $model, $key) {
+                                return Html::a('Ver', ['view', 'id' => $model->id], ['class' => 'btn btn-success']);
+                            },
+                            'delete' => function ($url, $model, $key) {
+                                return Html::a('Eliminar', ['delete', 'id' => $model->id], [
+                                    'class' => 'btn btn-danger',
+                                    'data' => [
+                                        'confirm' => '¿Estás seguro de que deseas eliminar esta estadística?',
+                                        'method' => 'post',
+                                    ],
+                                ]);
+                            },
+                        ],
+                    ],
+                ],
+            ]); ?>
+        </div>
+    </div>
+
+    <div class="marco">
+        <?= Html::a('Actualizar Estadísticas', ['actualizar-estadisticas'], ['class' => 'btn btn-primary']) ?>
+    </div>
+</body>
+</html>
+<?php } ?>
 
